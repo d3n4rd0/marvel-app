71d30a085e9bf11e7df211e822a559e33323cf29
unknown <denardo222@gmail.com>
2019-10-08 23:21:51 +0200

    adding comic and comics pages, comics grid style

--- src/components/Comic.vue
+++ src/components/Comic.vue
@@ -1,20 +1,91 @@
 <template>
-  <div class="comic">
-    Comic
+  <div class="comic-page">
+    <div class="detail-item-bg-wrap">
+      <div class="detail-item-bg">
+        <img :src="thumbUrl(comicData, 'portrait_uncanny')" width="633" height="537" alt="AMAZING FANTASY 15 FACSIMILE EDITION #1" title="AMAZING FANTASY 15 FACSIMILE EDITION #1">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Comic',
-  data () {
+  name: "Comic",
+  props:[
+    "id"
+  ],
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      loading: false,
+      comicData: {}
+    };
+  },
+  methods: {
+    thumbUrl(filename, type) {
+      if (filename.thumbnail) {
+        return (
+          filename.thumbnail.path + "/"+
+          type + "." +
+          filename.thumbnail.extension
+        );
+      }
+    },
+    getComic() {
+      this.loading = true;
+      let url =
+        "http://gateway.marvel.com/v1/public/comics?ts=1234&apikey=cbda9c62ecdcccbe91cfd88996a1dd50&hash=b981bf23bad169d54156ec8511f29f73&id="+this.id;
+      fetch(url)
+        .then(response => {
+          response.json().then(res => {
+            console.log(res);
+            this.comicData = res.data.results[0];
+            this.loading = false;
+          });
+        })
+        .catch(() => {
+          this.loading = false;
+        });
+    },
+    openComic(comic){
+      this.$router.push('/comic/'+comic.id)
+    }
+  },
+  mounted() {
+    this.getComic();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.comic-page {
+  .detail-item-bg-wrap {
+    display: block;
+    .detail-item-bg {
+      max-width: 100%;
+      margin: 0 auto;
+      pointer-events: none;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      width: 100%;
+      min-height: 100%;
+      overflow: hidden;
+      img {
+        width: 110% !important;
+        margin-right: -5%;
+        margin-top: -30px;
+        filter: blur(1rem);
+        height: auto !important;
+        background-color: transparent !important;
+        -webkit-mask-box-image: none !important;
+      }
+    }
+    .featured-item-info {
+        display: flex;
+        flex-wrap: wrap
     }
   }
 }
-</script>
-
-<style scoped>
 
 </style>
--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -1,20 +1,118 @@
 <template>
-  <div class="comics">
-    Comics
+  <div class="comics-page">
+    <h1 class="title">Comics</h1>
+    <div class="comics">
+      <div v-for="comic, index in comics" :key="comic.id" class="item">
+        <div class="" @click="openComic(comic)">
+          <div class="comic-title">{{comic.title}}</div>
+          <img :src="thumbUrl(comic)" />
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Comics',
-  data () {
+  name: "Comics",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      loading: false,
+      comics: []
+    };
+  },
+  methods: {
+    thumbUrl(filename) {
+      if (filename.thumbnail) {
+        return (
+          filename.thumbnail.path +
+          "/portrait_uncanny." +
+          filename.thumbnail.extension
+        );
+      }
+    },
+    getComics(limit, offset) {
+      this.loading = true;
+      let url =
+        "http://gateway.marvel.com/v1/public/comics?ts=1234&apikey=cbda9c62ecdcccbe91cfd88996a1dd50&hash=b981bf23bad169d54156ec8511f29f73";
+      if (limit) {
+        url += "&limit=" + limit;
+      }
+      if (offset) {
+        url += "&offset=" + offset;
+      }
+      fetch(url)
+        .then(response => {
+          response.json().then(res => {
+            console.log(res);
+            this.comics = res.data.results;
+            this.loading = false;
+          });
+        })
+        .catch(() => {
+          this.loading = false;
+        });
+    },
+    openComic(comic){
+      this.$router.push('/comic/'+comic.id)
     }
+  },
+  mounted() {
+    this.getComics();
   }
-}
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.comics-page {
+  .title {
+    margin: auto;
+    height: 52px;
+    line-height: 52px;
+    margin-left: 130px
+  }
+  .comics {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    background: rgba(114, 186, 94, 0.05);
+    margin-left: 130px;
+    justify-items: center;
+    align-items: center;
+    justify-content: space-evenly;
+    .item {
+      height: 500px;
+      width: 300px;
+      cursor: pointer;
+      opacity: 0.7;
+      .comic-title {
+        background-color: #ec1d24;
+        color:white;
+        width: 300px;
+        height: 40px;
+        
+      }
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+  @media (max-width: 1390px) {
+    .comics {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+  @media (max-width: 1090px) {
+    .comics {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+  @media (max-width: 790px) {
+    .comics {
+      grid-template-columns: 1fr;
+    }
+  }
+
+}
 
 </style>
